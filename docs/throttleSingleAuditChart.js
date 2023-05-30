3acb7998daf099911eb8e494b4d199d3712c04fd
M. Ali Koken <ali@koken-consulting.com>
2023-05-30 21:59:09 +0200

    checkpoint

--- docs/throttleSingleAuditChart.js
+++ docs/throttleSingleAuditChart.js
@@ -1,41 +1,29 @@
+// Upon selection of drowdownlist value, call setSessionChart value
+function setAuditChartData(applicationId, auditInstanceId) {
 
-
-$(document).ready(function () {
-    populateSessionDropDownListData(applicationId);
-    $('#dropdown').change(function () {
-        var sessionId = $(this).val();
-        setAuditChartData(applicationId, sessionId);
-    });
-});
-
-function populateSessionDropDownListData(applicationId) {
-    let url = constants.baseUrl + '/' + constants.reportsFolderName + '/' + applicationId + '/' + applicationId + constants.auditlistSuffix;
-    url= constants.reportsFolderName + '/' + applicationId + '/' + applicationId + constants.auditlistSuffix;
-    console.log("url: " + url);
-    // AJAX request to get dropdown data
-    $.ajax({
-        url: url,
-        type: 'GET',
-        success: function (data) {
-            // Populate dropdown with data
-            var dropdown = $('#dropdown');
-            //reset dropdown list 
-            dropdown.empty();
-            dropdown.append('<option selected="true" disabled>Choose Session</option>');
-            $.each(data[constants.AUDITS_NAME], function (key, value) {
-                //console.log(key + " " + JSON.stringify(value))
-                const ddOptionValue = value[constants.AUDIT_INSTANCE_ID];
-                const ddOptionKey = `${applicationId}-${value[constants.AUDIT_INSTANCE_ID]}-${value.endDateTime}`
-                dropdown.append($('<option></option>').attr('value', ddOptionValue).text(ddOptionKey));
-            });
-        },
-        error: function (data) {
-            console.log("error: " + data)
-            alert("Error getting session list data");
-        }
-    });
+    fetch(constants.reportsFolderName + '/' + applicationId + '/' + constants.AUDITS_NAME + '/' + auditInstanceId + '/' + constants.CHART_DATA_FOLDER_NAME + '/' + constants.ThrottleImpactReportFileName)
+        .then(response => response.json())
+        .then(data => processCPUSlowDownMultiplierResultsUponRequestSuccess(data, applicationId, auditInstanceId));
 }
 
+function processCPUSlowDownMultiplierResultsUponRequestSuccess(data, applicationId, auditInstanceId) {
+
+    var cpuSlowDownMultipliers = [];
+    var resultTypes = [];
+    generateOptionsForChart(cpuSlowDownMultipliers, resultTypes, applicationId, auditInstanceId);
+    generateDataForChartOptions(data, cpuSlowDownMultipliers, resultTypes);
+
+    var dataSetValues = [];
+    //given data: data.cpuSlowDownMultiplierResultsList.interactiveResult, loop
+    //through each key and create a dataset object
+    var currentColorIndex = 0;
+    currentColorIndex = processResultTypeInterActiveResult(data, currentColorIndex, dataSetValues);
+    currentColorIndex = processResultTypeSpeedIndex(data, currentColorIndex, dataSetValues);
+    //given data: data.cpuSlowDownMultiplierResultsList.speedIndex, loop
+    //through each key and create a dataset object
+    //keep same color counters
+    generateChartOnPage(currentColorIndex, data, dataSetValues, auditInstanceId);
+}
 function generateDataForChartOptions(data, cpuSlowDownMultipliers, resultTypes) {
     for (const [key, value] of Object.entries(data.cpuSlowDownMultiplierResultsList.interactiveResult)) {
         //console.log(key + " " + JSON.stringify(value))
@@ -63,24 +51,54 @@ function generateDataForChartOptions(data, cpuSlowDownMultipliers, resultTypes) 
 
 }
 
-function processCPUSlowDownMultiplierResultsUponRequestSuccess(data, applicationId, auditInstanceId) {
-
-    var cpuSlowDownMultipliers = [];
-    var resultTypes = [];
-    generateDataForChartOptions(data, cpuSlowDownMultipliers, resultTypes);
-    generateOptionsForChart(cpuSlowDownMultipliers, resultTypes, applicationId, auditInstanceId);
-
-    var dataSetValues = [];
-    //given data: data.cpuSlowDownMultiplierResultsList.interactiveResult, loop
-    //through each key and create a dataset object
-    var currentColorIndex = 0;
-    currentColorIndex = processResultTypeInterActiveResult(data, currentColorIndex, dataSetValues);
-    currentColorIndex = processResultTypeSpeedIndex(data, currentColorIndex, dataSetValues);
-    //given data: data.cpuSlowDownMultiplierResultsList.speedIndex, loop
-    //through each key and create a dataset object
-    //keep same color counters
-    generateChartOnPage(currentColorIndex, data, dataSetValues, auditInstanceId);
+function generateOptionsForChart(cpuSlowDownMultipliers, resultTypes, applicationId, sessionId) {
+    //run this function only if cpuSlowDownMultipliersCheckboxList innerHtml is empty
+    parseCPUSlowDownMultipliersCheckList(cpuSlowDownMultipliers, applicationId, sessionId);
+    parseResultTypeCheckList(resultTypes, applicationId, sessionId);
 }
+
+
+function processResultTypeInterActiveResult(data, currentColorIndex, dataSetValues) {
+    if (savedResultTypes.includes("interactiveResult")) {
+        for (const [key, value] of Object.entries(data.cpuSlowDownMultiplierResultsList.interactiveResult)) {
+            if (savedCpuSlowDownMultipliers.includes(key)) {
+                var dataSet = {
+                    label: "Interactive Result (" + key + "x CPU slowdown)",
+                    data: value,
+                    borderColor: colorList[currentColorIndex],
+                    fill: false
+                };
+                dataSetValues.push(dataSet);
+                currentColorIndex++;
+            }
+        }
+    }
+    return currentColorIndex;
+}
+
+function processResultTypeSpeedIndex(data, currentColorIndex, dataSetValues) {
+    //continue only if savedResultTypes contains speedIndex result type value
+    if (savedResultTypes.includes("speedIndex")) {
+        for (const [key, value] of Object.entries(data.cpuSlowDownMultiplierResultsList.speedIndex)) {
+            if (savedCpuSlowDownMultipliers.includes(key)) {
+                var dataSet = {
+                    label: "Speed Index (" + key + "x CPU slowdown)",
+                    data: value,
+                    borderColor: colorList[currentColorIndex],
+                    fill: false
+                };
+                dataSetValues.push(dataSet);
+                currentColorIndex++;
+            }
+        }
+    }
+    return currentColorIndex;
+}
+
+
+
+
+
 
 function generateChartOnPage(currentColorIndex, data, dataSetValues, sessionId) {
     //currentColorIndex = processResultTypeSpeedIndex(data, currentColorIndex, dataSetValues);
@@ -125,115 +143,4 @@ function generateChartOnPage(currentColorIndex, data, dataSetValues, sessionId) 
         options: chartOptions
     });
     return currentColorIndex;
-}
-
-function processResultTypeInterActiveResult(data, currentColorIndex, dataSetValues) {
-    if (savedResultTypes.includes("interactiveResult")) {
-        for (const [key, value] of Object.entries(data.cpuSlowDownMultiplierResultsList.interactiveResult)) {
-            if (savedCpuSlowDownMultipliers.includes(key)) {
-                var dataSet = {
-                    label: "Interactive Result (" + key + "x CPU slowdown)",
-                    data: value,
-                    borderColor: colorList[currentColorIndex],
-                    fill: false
-                };
-                dataSetValues.push(dataSet);
-                currentColorIndex++;
-            }
-        }
-    }
-    return currentColorIndex;
-}
-
-function processResultTypeSpeedIndex(data, currentColorIndex, dataSetValues) {
-    //continue only if savedResultTypes contains speedIndex result type value
-    if (savedResultTypes.includes("speedIndex")) {
-        for (const [key, value] of Object.entries(data.cpuSlowDownMultiplierResultsList.speedIndex)) {
-            if (savedCpuSlowDownMultipliers.includes(key)) {
-                var dataSet = {
-                    label: "Speed Index (" + key + "x CPU slowdown)",
-                    data: value,
-                    borderColor: colorList[currentColorIndex],
-                    fill: false
-                };
-                dataSetValues.push(dataSet);
-                currentColorIndex++;
-            }
-        }
-    }
-    return currentColorIndex;
-}
-
-// Upon selection of drowdownlist value, call setSessionChart value
-function setAuditChartData(applicationId, auditInstanceId) {
-
-    fetch(constants.reportsFolderName + '/' + applicationId + '/' + constants.AUDITS_NAME + '/' + auditInstanceId + '/' + constants.CHART_DATA_FOLDER_NAME + '/' + constants.ThrottleImpactReportFileName)
-        .then(response => response.json())
-        .then(data => processCPUSlowDownMultiplierResultsUponRequestSuccess(data, applicationId, auditInstanceId));
-}
-function generateOptionsForChart(cpuSlowDownMultipliers, resultTypes, applicationId, sessionId) {
-    //run this function only if cpuSlowDownMultipliersCheckboxList innerHtml is empty
-    parseCPUSlowDownMultipliersCheckList(cpuSlowDownMultipliers, applicationId, sessionId);
-    parseResultTypeCheckList(resultTypes, applicationId, sessionId);
-}
-
-function parseResultTypeCheckList(resultTypes, applicationId, sessionId) {
-    if (document.getElementById("typeOfValuesCheckboxList").innerHTML == "") {
-        // Create checkbox list for type of values (Interactive and Speed Index)
-        //var resultTypes = ["Interactive", "Speed Index"];
-        var typeOfResultsCheckboxList = "";
-        for (var i = 0; i < resultTypes.length; i++) {
-            typeOfResultsCheckboxList += '<input type="checkbox" class="resultTypeOptions" id="' + resultTypes[i] + '" name="typeOfValues" value="' + resultTypes[i] + '"';
-            if (savedResultTypes.includes(resultTypes[i])) {
-                typeOfResultsCheckboxList += ' checked';
-            }
-            typeOfResultsCheckboxList += '>';
-            typeOfResultsCheckboxList += '<label for="' + resultTypes[i] + '"> ' + resultTypes[i] + '</label><br>';
-        }
-        document.getElementById("typeOfValuesCheckboxList").innerHTML = typeOfResultsCheckboxList;
-        $(".resultTypeOptions").on('change', function () {
-            var value = $(this).val();
-            //remove the value from savedResultTypes array if it exists
-            if (savedResultTypes.includes(value)) {
-                var index = savedResultTypes.indexOf(value);
-                if (index > -1) {
-                    savedResultTypes.splice(index, 1);
-                }
-            } else {
-                savedResultTypes.push(value);
-            }
-            setApplicationChartData(applicationId, sessionId);
-        });
-    }
-}
-
-function parseCPUSlowDownMultipliersCheckList(cpuSlowDownMultipliers, applicationId, sessionId) {
-    if (document.getElementById("cpuSlowDownMultipliersCheckboxList").innerHTML == "") {
-
-        // Create checkbox list for CPU Slow Down Multipliers
-        // var cpuSlowDownMultipliers = [8, 4, 1, 0];
-        var cpuSlowDownMultipliersCheckboxList = "";
-        for (var i = 0; i < cpuSlowDownMultipliers.length; i++) {
-            cpuSlowDownMultipliersCheckboxList += '<input type="checkbox" class="cpuSlowDownMultiplierOptions" id="cpuSlowDownMultiplier' + cpuSlowDownMultipliers[i] + '" name="cpuSlowDownMultiplier" value="' + cpuSlowDownMultipliers[i] + '"';
-            if (savedCpuSlowDownMultipliers.includes(cpuSlowDownMultipliers[i])) {
-                cpuSlowDownMultipliersCheckboxList += ' checked';
-            }
-            cpuSlowDownMultipliersCheckboxList += '>';
-            cpuSlowDownMultipliersCheckboxList += '<label for="cpuSlowDownMultiplier' + cpuSlowDownMultipliers[i] + '"> ' + cpuSlowDownMultipliers[i] + 'x</label><br>';
-        }
-        document.getElementById("cpuSlowDownMultipliersCheckboxList").innerHTML = cpuSlowDownMultipliersCheckboxList;
-        $(".cpuSlowDownMultiplierOptions").on('change', function () {
-            var value = $(this).val();
-            //remove the value from savedCpuSlowDownMultipliers array if it exists
-            if (savedCpuSlowDownMultipliers.includes(value)) {
-                var index = savedCpuSlowDownMultipliers.indexOf(value);
-                if (index > -1) {
-                    savedCpuSlowDownMultipliers.splice(index, 1);
-                }
-            } else {
-                savedCpuSlowDownMultipliers.push(value);
-            }
-            setApplicationChartData(applicationId, sessionId);
-        });
-    }
 }
