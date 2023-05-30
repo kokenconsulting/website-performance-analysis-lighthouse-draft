6ac566cbe337bd92d3012631e9879f9da9bd0140
M. Ali Koken <ali@koken-consulting.com>
2023-05-30 13:26:35 +0200

    process runner removed from index.js for the time being

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import { AuditListReport } from './webApplication/AuditListReport.js';
 import { WebPageThrottledAuditConfiguration } from './webPageThrottledAudit/WebPageThrottledAuditConfiguration.js';
 import { ThrottlingSettings } from './throttling/ThrottlingSettings.js';
 import { WebPageThrottledAuditRunner } from './webPageThrottledAudit/WebPageThrottledAuditRunner.js';
-import { ProcessLogger } from './log/Processlogger.js';
 
 export {
   WebApplication,
-  ProcessLogger,
   WebPageThrottledAuditRunner,
   WebPageThrottledAuditEngine,
   AuditListReport,
