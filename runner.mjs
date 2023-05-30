a31d12acf4fa630122d2aca1aa8b5c5d33ef29eb
M. Ali Koken <ali@koken-consulting.com>
2023-05-30 13:15:38 +0200

    runner move.

--- runner.mjs
+++ runner.mjs
@@ -2,7 +2,7 @@ import * as path from 'path';
 import {
   WebPageThrottledAuditRunner,
 }
-  from '../src/index.js';
+  from './src/index.js';
 
 //get current working directory
 const configFullPath = path.join(process.cwd(), 'sample/config.json');
