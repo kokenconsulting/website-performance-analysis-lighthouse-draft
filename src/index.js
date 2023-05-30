946554a54cc8f51f7fa305ccded02b3e07d0704c
M. Ali Koken <ali@koken-consulting.com>
2023-05-30 13:25:20 +0200

    reorder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-
 import { WebApplication } from './webApplication/WebApplicationModel.js';
-import { ProcessLogger } from './log/Processlogger.js';
 import { WebPageThrottledAuditEngine } from './webPageThrottledAudit/WebPageThrottledAuditEngine.js';
 import { AuditListReport } from './webApplication/AuditListReport.js';
 import { WebPageThrottledAuditConfiguration } from './webPageThrottledAudit/WebPageThrottledAuditConfiguration.js';
 import { ThrottlingSettings } from './throttling/ThrottlingSettings.js';
-import {WebPageThrottledAuditRunner} from './webPageThrottledAudit/WebPageThrottledAuditRunner.js';
+import { WebPageThrottledAuditRunner } from './webPageThrottledAudit/WebPageThrottledAuditRunner.js';
+import { ProcessLogger } from './log/Processlogger.js';
 
 export {
   WebApplication,
