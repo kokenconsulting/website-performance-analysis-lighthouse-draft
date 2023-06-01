7ff71102aa2c512f337eadadc51e76d25fb12a79
M. Ali Koken <ali@koken-consulting.com>
2023-06-01 08:03:44 +0200

    config updates.

--- runner.mjs
+++ runner.mjs
@@ -4,7 +4,25 @@ import {
 }
   from './src/index.js';
 
-//get current working directory
-const configFullPath = path.join(process.cwd(), 'throttedRunConfig.json');
-const auditRunner = new WebPageThrottledAuditOrchestrator(configFullPath);
-await auditRunner.run(true);
+async function GoogleSupportLP() {
+  //get current working directory
+  const configFullPath = path.join(process.cwd(), 'auditConfigurations/Google-Support-LP.json');
+  const auditRunner = new WebPageThrottledAuditOrchestrator(configFullPath);
+  await auditRunner.run(true);
+}
+async function MagicRatingLandingL6(){
+  //get current working directory
+  const configFullPath = path.join(process.cwd(), 'auditConfigurations/MagicRating-Landing-L6.json');
+  const auditRunner = new WebPageThrottledAuditOrchestrator(configFullPath);
+  await auditRunner.run(true);
+}
+async function MagicRatingLandingLP(){
+  //get current working directory
+  const configFullPath = path.join(process.cwd(), 'auditConfigurations/MagicRating-Landing-LP.json');
+  const auditRunner = new WebPageThrottledAuditOrchestrator(configFullPath);
+  await auditRunner.run(true);
+}
+
+await GoogleSupportLP();
+await MagicRatingLandingL6();
+await MagicRatingLandingLP();
